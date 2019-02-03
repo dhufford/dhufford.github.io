b70033637596efd6672138980f4b81174b89fc13
dhufford <37636086+dhufford@users.noreply.github.com>
2019-02-02 20:23:49 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from data import Articles
+
+app = Flask(__name__)
+
+Articles = Articles()
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/photos')
+def photos():
+    return render_template('photos.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles=Articles)
+
+
+@app.route('/article/<string:author>/')
+def article(author):
+    return render_template('article.html', author=author)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles = [
+
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+            'author': 'Dale Hufford',
+            'create_date': '31-01-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+            'author': 'Paul Powel',
+            'create_date': '31-01-2019'
+
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+            'author': 'Paul Washer',
+            'create_date': '31-01-2019'
+
+        }
+    ]
+    return articles
